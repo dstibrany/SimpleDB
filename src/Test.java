be10074e990811c15aa18734b5c448e35436a90f
Dave Stibrany <daves@hubdoc.com>
2017-12-28 18:18:01 -0500

    update test file; lab1 done!

--- src/Test.java
+++ src/Test.java
@@ -12,7 +12,7 @@ public class Test {
 
         // create the table, associate it with some_data_file.dat
         // and tell the catalog about the schema of this table.
-        HeapFile table1 = new HeapFile(new File("some_data_file.dat"), descriptor);
+        HeapFile table1 = new HeapFile(new File("data/table_3_col.dat"), descriptor);
         Database.getCatalog().addTable(table1, "test");
 
         // construct the query: we use a simple SeqScan, which spoonfeeds

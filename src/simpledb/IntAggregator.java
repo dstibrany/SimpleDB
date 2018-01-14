b5552e94a3dfcd49242725daeb60be3f808b2469
Dave Stibrany <daves@hubdoc.com>
2018-01-14 15:52:28 -0500

    add comments

--- src/simpledb/IntAggregator.java
+++ src/simpledb/IntAggregator.java
@@ -44,12 +44,23 @@ public class IntAggregator implements Aggregator {
         groupAggregates.put(groupByField, updatedAggregate);
     }
 
+    /**
+     * Returns the updated count aggregate (+1 for each tuple we've seen in a particular group)
+     * @param tup The tuple used to update the current count
+     * @return the updated count
+     */
     private int getUpdatedCount(Tuple tup) {
         Field groupByField = getGroupByField(tup);
         int currentCount = groupCounts.getOrDefault(groupByField, 0);
         return ++currentCount;
     }
 
+    /**
+     * Returns the updated aggregate value, by adding the tuple aggregate value to the group aggregate value
+     * @param tup The current tuple whose aggregate value we will merge into the group
+     * @param currentCount The number of tuples we have seen in this group
+     * @return the new aggregate value
+     */
     private int getUpdatedAggregate(Tuple tup, int currentCount) {
         IntField tupleAggregate = (IntField)tup.getField((this.aggField));
         Field groupByField = getGroupByField(tup);
@@ -77,6 +88,11 @@ public class IntAggregator implements Aggregator {
         return updatedAggregateValue;
     }
 
+    /**
+     * Returns the groupby Field of the current tuple, or null if there is no grouping
+     * @param tup The tuple whose groupby Field we wish to return
+     * @return the Field used in the group by clause, or null if there is no grouping
+     */
     private Field getGroupByField(Tuple tup) {
         Field groupByField;
         if (this.gbField == Aggregator.NO_GROUPING) {
@@ -109,6 +125,8 @@ public class IntAggregator implements Aggregator {
         for (HashMap.Entry<Field, Integer> groupAggregateEntry: this.groupAggregates.entrySet()) {
             Tuple groupAggregateTuple = new Tuple(groupAggregateTd);
 
+            // If there is a grouping, we return a tuple in the form {groupByField, aggregateVal}
+            // If there is no grouping, we return a tuple in the form {aggregateVal}
             if (hasGrouping) {
                 groupAggregateTuple.setField(0, groupAggregateEntry.getKey());
                 groupAggregateTuple.setField(1, new IntField(groupAggregateEntry.getValue()));
